af4152810c3b85339d9d25a3783473ef235eba80
edrikfsteiner <edrikfsteiner@gmail.com>
2025-02-10 22:11:10 -0300

    Added handlers

---
+++ api-phonebook/src/main/java/com/phonebook/handler/ExceptionDetails.java
@@ -0,0 +1,14 @@
+package com.phonebook.handler;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ExceptionDetails {
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String details;
+}
