eff8844ae817b9a0bf090aa0b034794ac7f84f25
edrikfsteiner <edrikfsteiner@gmail.com>
2025-03-17 21:55:34 -0300

    First commit

---
+++ src/test/java/com/phonebook/PhonebookApplicationTests.java
@@ -0,0 +1,11 @@
+package com.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
